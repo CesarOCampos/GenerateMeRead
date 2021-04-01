914cd2574dc218fa15ae4c8f1e469c17751c8e83
Cesar Campos <cesarorlandocampos@gmail.com>
2021-03-31 23:47:39 -0500

    saving so far and still need to finish index but markdown finished majority

---
+++ index.js
@@ -0,0 +1,85 @@
+// Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+// Create an array of questions for user input
+const questions = [{
+        type: 'input',
+        message: `What is your project's title?`,
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: `Please write a short description of your project:`,
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'installation',
+        default: 'npm install',
+    },
+    {
+        type: 'list',
+        message: 'What kind of license should your project have?',
+        name: 'license',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using the repo?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about contributing to the repo?',
+        name: 'contribution',
+    }, {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        name: 'test',
+        default: 'npm test',
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+]
+
+.catch(error => {
+    if (error.isTtyError) {
+        console.log("You must use a different application to access this program.");
+        // Prompt couldn't be rendered in the current environment
+    } else {
+        // Something else went wrong
+        console.log("An error has occurred.");
+    }
+});
+}
+// Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), function(err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+}
+
+// Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        console.log(JSON.stringify(data, null, " "));
+        data.getLicense = getLicense(data.license);
+        writeToFile("./utils/README.md", data);
+    });
+}
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,75 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+    switch (license) {
+        case 'MIT':
+            return (`[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`);
+        case 'APACHE 2.0':
+            return (`[![License](https://img.shields.io/badge/License-Apache%202.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0)`);
+        case 'GPL 3.0':
+            return (`[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`);
+        case 'BSD 3':
+            return (`[![License](https://img.shields.io/badge/License-BSD%203--Clause-orange.svg)](https://opensource.org/licenses/BSD-3-Clause)`);
+        case 'None':
+            return ('');
+    }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+    switch (license) {
+        case 'MIT':
+            return (`[MIT](https://opensource.org/licenses/MIT)`);
+        case 'APACHE 2.0':
+            return (`[Apache 2.0](https://opensource.org/licenses/Apache-2.0)`);
+        case 'GPL 3.0':
+            return (`[GPL v3](https://www.gnu.org/licenses/gpl-3.0)`);
+        case 'BSD 3':
+            return (`[BSD 3](https://opensource.org/licenses/BSD-3-Clause)`);
+        default:
+            return ('');
+    }
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+    (license) ? (`${renderLicenseBadge(license)} This project is licensed under the ${renderLicenseLink(license)} license.`) : ("");
+}
+
+// TODO: Create a function to generate markdown for README
+
+function generateMarkdown(results) {
+    return `# ${results.title}
+#### Table of contents
+    1. [Project Description](#project-description)
+    2. [Installation Instructions](#installation-instructions)
+    3. [Usage Information](#usage-information)
+    4. [Contributor Guidelines](#contributor-guidelines)
+    5. [Test Instructions](#test-instructions)
+    6. [License](#license)
+    7. [Questions](#questions)
+    ## Project Description
+    * ${results.description}
+    
+    ## Installation Instructions
+    * ${results.installation}
+    
+    ## Usage Information
+    * ${results.usage}
+    
+    ##Contributor Guidlines
+    * ${results.contribution}
+    
+    ## Test Instructions
+    * ${results.test}
+    
+    ## License
+    * ${renderLicenseBadge(results)}
+    
+    ## Questions
+    * Emails me at ${results.email}
+    * Find me on GitHub [${results.github}](http://github.com/${results.github})`;
+}
+module.exports = generateMarkdown;
